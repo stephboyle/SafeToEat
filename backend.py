60cd41ba95265925ce36e345007caa8bdd626dab
Boyle-S47 <Boyle-S47@dev.azure.com>
2023-01-09 00:42:29 +0000

    error with login

--- backend.py
+++ backend.py
@@ -13,6 +13,8 @@ from functools import wraps
 #from importlib_metadata import re
  
 app = Flask(__name__)
+
+app.config['SECRET KEY'] = 'mysecret'
  
 client = MongoClient("mongodb://127.0.0.1:27017")
 
@@ -25,14 +27,31 @@ blacklist = db.blacklist
 # def index():
 #     return make_response("<h1>Hello World</h1>", 200)
 
+@app.route("/api/v1.0/users", methods=["GET"])
+#@jwt_required
+def show_all_users():
+    page_num, page_size = 1, 10
+    if request.args.get('pn'):
+        page_num = int(request.args.get('pn'))
+    if request.args.get('ps'):
+        page_size = int(request.args.get('ps'))
+    page_start = (page_size * (page_num - 1))
+ 
+    data_to_return = []
+    for user in users.find().skip(page_start).limit(page_size):
+        user['_id'] = str(user['_id'])
+        for details in users.find():
+            details['_id'] = str(details['_id'])
+        data_to_return.append(user)    
+ 
+    return make_response( jsonify( data_to_return ), 200)
+
 # get user
-@app.route("/api/v1.0/businesses/<string:id>", methods=["GET"])
-def show_one_user(id):
-    user = users.find_one({'_id':ObjectId(id)})
+@app.route("/api/v1.0/users/<_id>", methods=["GET"])
+def show_one_user(_id):
+    user = users.find_one({'_id':ObjectId(_id)})
     if user is not None:
-        # user["_id"] = str(user['_id'])
-        # for review in business['reviews']:
-        #     review['_id'] = str(review['_id'])
+        user["_id"] = str(user['_id'])
         return make_response( jsonify( user ), \
          200 )
     else:
@@ -41,38 +60,61 @@ def show_one_user(id):
 # creating account
 @app.route("/api/v1.0/users", methods=["POST"])
 def add_user():
-    next_id = users[-1]["id"] + 1
-    new_user = { "_id": next_id,
-                 "first_name": request.form["first_name"],
-                 "last_name": request.form["last_name"],
-                 "email_address": request.form["email_address"],
-                 "password": request.form["password"],
-                 "allergy": request.form["allergy"]
-                 }
-    users.append(new_user)
-    return make_response( jsonify( new_user ), 201) 
+    if "first_name" in request.form and "last_name" in request.form and "email_address" in request.form and "password" in request.form and "allergy" in request.form:
+        new_user = {"first_name": request.form["first_name"],
+                    "last_name": request.form["last_name"],
+                    "email_address": request.form["email_address"],
+                    "password": request.form["password"],
+                    "allergy": request.form["allergy"]
+                    }
+        new_user_id = users.insert_one(new_user)
+        new_user_link = "http://localhost:5000/api/v1.0/users/" + str(new_user_id.inserted_id)
+        return make_response( jsonify( { "url" : new_user_link}), 201)
+
+    # users.append(new_user)
+    # return make_response( jsonify( new_user ), 201) 
 
 # editing account details
-@app.route("/api/v1.0/users/<int:id>", methods=["PUT"])
-def edit_user(id):
-    for user in users:
-        if user["id"] == id:
-            user["first_name"] = request.form["first_name"]
-            user["last_name"]: request.form["last_name"]
-            user["email_address"]: request.form["email_address"]
-            user["password"]: request.form["password"]
-            user["allergy"]: request.form["allergy"]
-            break
-    return make_response( jsonify( user ), 200)
+@app.route("/api/v1.0/users/<_id>", methods=["PUT"])
+def edit_user(_id):
+    if "first_name" in request.form and "last_name" in request.form and "email_address" in request.form and "password" in request.form and "allergy" in request.form:
+        result = users.update_one(
+            { "_id" : ObjectId(_id) }, {
+                "$set" : { "first_name" : request.form["first_name"],
+                            "last_name": request.form["last_name"],
+                            "email_address": request.form["email_address"],
+                            "password": request.form["password"],
+                            "allergy": request.form["allergy"]}
+            }
+        )
+        if result.matched_count == 1:
+            edited_user_link = "http://localhost:5000/api/v1.0/users/" + _id
+            return make_response( jsonify( { "url":edited_user_link }), 200)
+#     for user in users:
+#         if user["id"] == id:
+#             user["first_name"] = request.form["first_name"]
+#             user["last_name"]: request.form["last_name"]
+#             user["email_address"]: request.form["email_address"]
+#             user["password"]: request.form["password"]
+#             user["allergy"]: request.form["allergy"]
+#             break
+#     return make_response( jsonify( user ), 200)
+
+
 
 # deleting account
-@app.route("/api/v1.0/users/<int:id>", methods=["DELETE"])
-def delete_user(id):
-    for user in users:
-        if user["id"] == id:
-            users.remove(user)
-            break
-    return make_response( jsonify( {} ), 200)
+@app.route("/api/v1.0/users/<_id>", methods=["DELETE"])
+def delete_user(_id):
+    result = users.delete_one( { "_id" : ObjectId(_id)})
+    if result.deleted_count == 1:
+        return make_response( jsonify( {} ), 204)
+    else:
+        return make_response( jsonify( {"error": "Invalid user ID"}), 404)
+    # for user in users:
+    #     if user["id"] == id:
+    #         users.remove(user)
+    #         break
+    # return make_response( jsonify( {} ), 200)
 
 # # auth user login
 # @app.route('/api/v1.0/login', methods=['GET'])
@@ -96,8 +138,7 @@ def delete_user(id):
 def login():
     auth = request.authorization
     if auth and auth.password == 'password':
-        token = jwt.encode( \
-            {'user' : auth.username, \
+        token = jwt.encode( {'username' : auth.username, \
              'exp' : datetime.datetime.utcnow() + \
                     datetime.timedelta(minutes=30)}, \
             app.config['SECRET_KEY'])
